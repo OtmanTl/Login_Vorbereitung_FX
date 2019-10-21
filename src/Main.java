b193a52b61412958933dcd1688744ce0222c8c0e
OtmanTl <smottoul@gmail.com>
2019-10-21 23:22:53 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,111 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Reflection;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @web http://zoranpavlovic.blogspot.com/
+ */
+public class Main extends Application {
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle(" Login");
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("images.png")));
+
+        BorderPane bp = new BorderPane();
+        bp.setPadding(new Insets(10,50,50,50));
+
+        //Adding HBox
+        HBox hb = new HBox();
+        hb.setPadding(new Insets(20,20,20,30));
+
+        //Adding GridPane
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(20,20,20,20));
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
+
+        //Implementing Nodes for GridPane
+        Label lblUserName = new Label("User :");
+        final TextField txtUserName = new TextField();
+        Label lblPassword = new Label("Password :");
+        final PasswordField pf = new PasswordField();
+        Button btnLogin = new Button("Sign in");
+        final Label lblMessage = new Label();
+
+        //Adding Nodes to GridPane layout
+        gridPane.add(lblUserName, 0, 0);
+        gridPane.add(txtUserName, 1, 0);
+        gridPane.add(lblPassword, 0, 1);
+        gridPane.add(pf, 1, 1);
+        gridPane.add(btnLogin, 2, 1);
+        gridPane.add(lblMessage, 1, 2);
+
+
+        //Reflection for gridPane
+        Reflection r = new Reflection();
+        r.setFraction(0.7f);
+        gridPane.setEffect(r);
+
+        //DropShadow effect 
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setOffsetX(5);
+        dropShadow.setOffsetY(5);
+
+        //Adding text and DropShadow effect to it
+        Text text = new Text("Join US");
+        text.setFont(Font.font("Courier New", FontWeight.BOLD, 28));
+        text.setEffect(dropShadow);
+
+        //Adding text to HBox
+        hb.getChildren().add(text);
+
+        //Add ID's to Nodes
+        bp.setId("bp");
+        gridPane.setId("root");
+        btnLogin.setId("btnLogin");
+        text.setId("text");
+
+
+
+
+        //Add HBox and GridPane layout to BorderPane Layout
+        bp.setTop(hb);
+        bp.setCenter(gridPane);
+
+        //Adding BorderPane to the scene and loading CSS
+        Scene scene = new Scene(bp);
+        scene.getStylesheets().add(getClass().getClassLoader().getResource("login.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.titleProperty().bind(
+                scene.widthProperty().asString().
+                        concat(" : ").
+                        concat(scene.heightProperty().asString()));
+        //primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
